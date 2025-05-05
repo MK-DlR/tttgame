5793a0e0ec9c066b5bf2d7ce370f5b4a05f19549
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-05-05 12:45:43 -0400

    Create various gameplay functions
    Create getValue function to retrieve current value of cell
    Create Cell function to take player markers
    Create printBoard function to print to console
    Create dropMarker function to check if chosen cell is available

--- tttgamescript.js
+++ tttgamescript.js
@@ -13,21 +13,64 @@ all functionality should fit in the various objects (game, player, gameboard)
 const createGame = (function () {
   const rows = 3;
   const columns = 3;
-  const gameBoard = []; // create gameboard array
-  console.log("gameBoard goes here"); // testing
+  const board = []; // create gameboard array
 
-  // loop to create 2D array
+  // nested for loop to create 2D array for gameboard
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
       board[i].push(Cell());
     }
   }
-  return gameBoard;
+
+  // get entire gameboard
+  const getBoard = () => board;
+
+  // drop player marker
+  const dropMarker = (column, player) => {
+    // check available cells
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+    if (!availableCells.length) return; // if no available cells, move is invalid
+    board[row][column].addMarker(player); // if cell is valid, drop player marker
+  };
+
+  // print board to console
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return { board, dropMarker, printBoard }; // interface for application to interact with the board
 })();
 
+/*
+squares on the board
+0: empty
+1: player 1's marker
+2: player 2's token
+*/
+function Cell() {
+  let value = 0;
+  // accept player marker to change value
+  const addMarker = (player) => {
+    value = player;
+  };
+
+  // retrieve current value of cell
+  const getValue = () => value;
+
+  return {
+    addMarker,
+    getValue,
+  };
+}
+
 // factory to create players
-function createPlayer(name, marker) {
+function player(name, marker) {
   const playerName = name;
   const playerMarker = marker;
   let playerScore = 0; // testing
@@ -37,7 +80,7 @@ function createPlayer(name, marker) {
 }
 
 // creating test player
-const adrien = createPlayer("adrien", "X");
+const adrien = player("adrien", "X");
 adrien.giveScore();
 
 // logging test player info
